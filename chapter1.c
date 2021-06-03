f5a8ff1bceceb3da2d2fcee8fda1514fbad1a458
hongrthing <41066032+hongrthing@users.noreply.github.com>
2021-06-03 16:02:00 +0900

    Create chapter1.c

---
+++ chapter1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+//c표준 라이브러리 중 하나인 stdio.h 헤더파일에 선언된 내용을 포함시킴
+#include <stdlib.h> 
+//c표준 라이브러리 중 하나인 stdlib.h 헤더파일에 선언된 내용을 포함시킴
+int main(void) 
+//main 시작함수로 시작 
+{
+	printf("Hello world\n"); 
+	//Hello world를 출력하고 줄바꿈  
+	system("pause"); 
+	//pause시스템 명령을 수행 (콘솔창 일시정지기능) 
+	return  0; 
+	//main 함수 종료 
+}
